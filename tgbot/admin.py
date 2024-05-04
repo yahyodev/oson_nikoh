48fdd3fdc3a48922bcc43fa6762f8f668b775f50
Yakhyobek Abdunazarov <itstoolongname@gmail.com>
2024-05-04 23:50:35 +0100

    feat: admin + complaints

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from tgbot.models import User as TelegramUser, Complaint, NecessaryLink, Referral
+from django.db.models import Count
 
 
 @admin.register(TelegramUser)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "full_name", "username", "telegram_id",)
-    search_fields = ("full_name", "username", "telegram_id",)
+    list_display = ("id", "name", "location", "username", "telegram_id", 'status', 'complaint_count')
+    search_fields = ("full_name", "username", "telegram_id", "location", "name")
+
+    def get_queryset(self, request):
+        # Prefetch the complaint count with each user to avoid N+1 queries problem
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _complaint_count=Count("accused")
+        )
+        return queryset
+
+    def complaint_count(self, obj):
+        # Utilize the annotated value for performance
+        return obj._complaint_count
+
+    complaint_count.admin_order_field = '_complaint_count'  # Allows column order sorting
+    complaint_count.short_description = 'Complaints Received'  # Renames column head
 
 
 @admin.register(Complaint)
